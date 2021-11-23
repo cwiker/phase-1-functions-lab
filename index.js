ccf33dcefcc081ad23752dd088d8e97a0e58ab8c
Chris Wiker <cwiker7@gmail.com>
2021-11-23 10:26:43 -0500

    commit tree 71a03ecd7861027358dbbba301a41f2df89d425e

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickup) {
+
+const headquaters = 42;
+const distanceToFortyTwo = Math.abs(pickup - headquarters);
+return distanceToFortyTwo;
+}
+
+function distanceFromHqInFeet(pickup) {
+    distanceFromHqInBlocks(pickup);
+    const blocks = distanceFromHqInBlocks(pickup) * 264;
+    return blocks;
+}
+
+function distanceTravelledInFeet(start, destination) {
+
+    const travel = (destination - start) * 264;
+    return Math.abs(travel);
+}
+
+function calculatesFarePrice(start, destination) {
+
+    const feet = distanceTravelledInFeet(start, destination);
+
+        if (feet <= 400) {
+            return 0;
+        }   else if (feet > 400 && feet <= 2000) {
+            return (feet - 400) * .02;
+        }   else if (feet > 2500) {
+            return 'cannot travel that far';
+        }   else if (feet > 2000) {
+            return 25;
+        }
+    }
